ab695e1fa773f6fd5dd77503985d24668f49491b
SergejShk <sshkurupij11@gmail.com>
2022-12-08 03:14:59 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import NewComp from "./components/NewComp";
+
+const App: React.FC = () => {
+  return <NewComp />;
+};
+
+export default App;
---
+++ src/components/NewComp.tsx
@@ -0,0 +1,5 @@
+const NewComp: React.FC = () => {
+  return <div>Hello my friend!</div>;
+};
+
+export default NewComp;
